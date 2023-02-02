cc9bfec76285754551c20ce6de5ca87989cf3571
Diêgo Claudiano <dgsilva.230493@gmail.com>
2023-02-01 22:14:23 -0300

    iniciando banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,3 @@
-const { Statement } = require("sqlite3");
-
 // Importar a dependência do sqlite3
 const sqlite3 = require("sqlite3").verbose()
 
@@ -7,39 +5,74 @@ const sqlite3 = require("sqlite3").verbose()
 const db = new sqlite3.Database("./src/database/database.db")
 
 // Utilizar o objeto de banco de dados, para nossas operações
-db.serialize(() => {
+// db.serialize(() => {
 
   // Com comandos SQL wu vou:
 
   // 1 - Criar uma tabela
-  db.run(`
-    CREATE TABLE IF NOT EXISTS places (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      image TEXT,
-      name TEXT,
-      address TEXT,
-      address2 TEXT,
-      state TEXT,
-      city TEXT,
-      items TEXT
-    );
-  `)
+  // db.run(`
+  //   CREATE TABLE IF NOT EXISTS places (
+  //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+  //     image TEXT,
+  //     name TEXT,
+  //     address TEXT,
+  //     address2 TEXT,
+  //     state TEXT,
+  //     city TEXT,
+  //     items TEXT
+  //   );
+  // `)
 
-  // 2 - Inserir dados na tabela
-  const query = `
-      INSERT INTO places (
-        image,
-        name,
-        address
-        address2
-        Statement,
-        city,
-        items
-      ) VALUES (?,?,?,?,?,?,?);
-    `
-  db.run(query)
+  // // 2 - Inserir dados na tabela
+  // const query = `
+  //     INSERT INTO places (
+  //       image,
+  //       name,
+  //       address,
+  //       address2,
+  //       state,
+  //       city,
+  //       items
+  //     ) VALUES (?,?,?,?,?,?,?);
+  //   `
 
-  // 3 - Consultar os dados da tabela
+  // const values = [
+  //   "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //   "Papersider",
+  //   "Guilherme Gemballa, Jardim América",
+  //   "Nº 260",
+  //   "Santa Catarina",
+  //   "Rio do Sul",
+  //   "Papéis e Papelão"
+  // ]
+  
+  // function afterInsertData(err) {
+  //   if(err) {
+  //     return console.log(err)
+  //   }
+
+  //   console.log("Cadastrado com sucesso!")
+  //   console.log(this)
+  // }
+
+  // db.run(query, values, afterInsertData)
+
+    // 3 - Consultar os dados da tabela
+    // db.all(`SELECT name FROM places`, function(err, rows) {
+    //   if(err) {
+    //     return console.log(err)
+    //   }
+  
+    //   console.log("Aqui estão seus registros: ")
+    //   console.log(rows)
+    // })
 
   // 4 - Deletar um dado da tabela
-})
+  // db.run(`DELETE FROM places WHERE id = ?`, [2], function(err) {
+  //   if(err) {
+  //     return console.log(err)
+  //   }
+
+  //   console.log("Registro deletado com sucesso!")
+  // })
+// })
