4a4ce79a76088d6e6752497b99ee1d3742f67b32
Diêgo Claudiano <dgsilva.230493@gmail.com>
2023-02-15 21:49:11 -0300

    habilitando o BD para cadastro

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ const db = require("./database/db")
 // configurar pasta publica
 server.use(express.static("public"))
 
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({extended: true}))
+
 
 // utilizando template engine
 const nunjucks = require("nunjucks")
@@ -25,9 +28,59 @@ server.get("/", (req, res) => {
   return res.render("index.html", { title: "Um título" })
 })
 
+
+
 server.get("/create-point", (req, res) => {
+
+  //req.query: query strings da nossa url
+  // console.log(req.query)
+
   return res.render("create-point.html")
 })
+
+server.post("/savepoint", (req, res) => {
+
+  // req.body: O corpo do nosso formulário
+  // console.log(req.body)
+
+  // inserir dados no banco de dados
+  const query = `
+      INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+      ) VALUES (?,?,?,?,?,?,?);
+    `
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+  
+  function afterInsertData(err) {
+    if(err) {
+      return console.log(err)
+    }
+
+    console.log("Cadastrado com sucesso!")
+    console.log(this)
+
+    return res.send("ok")
+  }
+
+  db.run(query, values, afterInsertData)
+
+})
+
 
 server.get("/search", (req, res) => {
 
